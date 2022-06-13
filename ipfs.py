883ab707b99a493940891646a70b15ff4b02dfe7
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-06-13 02:23:21 -0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -16,8 +16,7 @@ def pin_to_ipfs(data):
 
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files = files, auth = auth)
 	
-	cid = response.text
-	
+	cid = response['Hash']
 	
 	print("1.9")
 	print(cid)

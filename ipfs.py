f9b88bf05ea2aed9421de0cc12553a71164daf83
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-06-13 18:11:13 -0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -6,11 +6,7 @@ def pin_to_ipfs(data):
 	#YOUR CODE HERE
 
 	json_object = json.dumps(data, indent = 4) 
-	
-	files = {
-		'file': json_object,
-	}
-
+	files = {'file': json_object,}
 	auth = ("2AW1mnzV6tcq27eNtvbTl3cgPXW","4db7b25efb64a0104b2ec86b7cc6ba77")
 
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files = files, auth = auth)
@@ -18,27 +14,17 @@ def pin_to_ipfs(data):
 	p = response.json()
 	cid = p['Hash']
 	
-	print(cid)
-
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
 	
-	print(cid)
-	
 	params = (('arg', cid),)
-	
 	auth = ("2AW1mnzV6tcq27eNtvbTl3cgPXW","4db7b25efb64a0104b2ec86b7cc6ba77")
 	
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth = auth)
-	
-	print(response)
-	print(type(response))
-	
 	data = response.json()
-	print(data)
 	
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
